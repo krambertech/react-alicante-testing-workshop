4aa5927b0640460242e4dbbcd91cc8843d6206e9
Kateryna Porshnieva <kateryna.porshnieva@veriff.net>
2022-09-29 01:01:19 +0200

    remove unnecessary readme file

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e",
-    setupNodeEvents(on, config) {
-      config.baseUrl = "http://localhost:3000";
-      return config;
-    },
+    baseUrl: "http://localhost:3000",
   },
 });
