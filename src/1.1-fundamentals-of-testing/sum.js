5fca3e7289c4afa9da9a1573c99e533d5862a07d
Kateryna Porshnieva <kateryna.porshnieva@veriff.net>
2022-09-29 11:56:12 +0200

    cleanup sum

--- src/1.1-fundamentals-of-testing/sum.js
+++ src/1.1-fundamentals-of-testing/sum.js
@@ -1,6 +1,14 @@
 const sum = (...numbers) => {
-  return numbers.reduce((result, number) => result + number, 0);
+  let result = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
+  }
+
+  return result;
 };
 
 sum(2, 3); // => 5
 sum(2, 3, 4); // => 9
+
+// module.exports = sum;
